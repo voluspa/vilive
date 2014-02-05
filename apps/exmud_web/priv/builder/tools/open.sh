b1639a47c9eb19efa95062ec7f3069e4522d558c
Sean Copenhaver <sean.copenhaver@gmail.com>
2014-02-05 13:36:13 -0500

    using a lot of docs in my browser so add those, also just in case want testem to complain if it doesn't find any tests

--- apps/exmud_web/priv/builder/tools/open.sh
+++ apps/exmud_web/priv/builder/tools/open.sh
@@ -2,3 +2,5 @@
 open -a '/Applications/Google Chrome.app' 'http://localhost:3000/builder'
 open -a '/Applications/Google Chrome.app' 'http://emberjs.com/api/'
 open -a '/Applications/Google Chrome.app' 'http://threejs.org/docs/'
+open -a '/Applications/Google Chrome.app' 'http://chaijs.com/api/bdd/'
+open -a '/Applications/Google Chrome.app' 'http://www.openbsd.org/cgi-bin/man.cgi?query=tmux&sektion=1'
