51c177b1e20db63bad7b41d21ffe9dc73abc26b8
Sean Copenhaver <sean.copenhaver@gmail.com>
2014-02-11 22:48:21 -0500

    added a quick test to cover the ability to click a room in the world viewer and get the form to pop up

--- apps/exmud_web/priv/builder/spec/views/app_spec.js
+++ apps/exmud_web/priv/builder/spec/views/app_spec.js
@@ -31,8 +31,8 @@ describe('the world view', function () {
                   "title": "A dark cave",
                   "description": "Seriously bring a torch.",
                   "location": {
-                    "x": 1,
-                    "y": 1,
+                    "x": 0,
+                    "y": 0,
                     "z": 0
                   },
                   "id": 1
@@ -48,6 +48,22 @@ describe('the world view', function () {
 
         it('renders the world', function () {
             expect(find('.world-viewer')).to.have.length(1);
+        });
+
+        describe('when a user clicks a room', function () {
+            var renderer = require('app/lib/world_renderer').getRenderer();
+
+            beforeEach(function () {
+                //gotta size it ourselves so that we know how many pixels we are working with
+                renderer.resize(500, 500);
+                //select the room
+                renderer.mouse2d(250, 250);
+                click('div.world-viewer');
+            });
+
+            it('displays the room form', function () {
+                expect(find('form.room')).to.have.length(1);
+            });
         });
     });
 });
