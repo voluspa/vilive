aa6b04156d5ab4a44f136ecfc8ecf9d20ceee4a2
Matthew Philyaw <matthew.philyaw@gmail.com>
2014-04-18 23:35:56 -0400

    I think this will be enough to get us started.
    See usuage details from `--help`.
    
    Current takes an input directory and outputs a single file defined by the out argument with the contents of the compiled templates.
    
    next phase will probably be to split this out into multiple files, but I think that's going to take a little more thought than this version.

---
+++ tools/compile-templates.js
@@ -0,0 +1,47 @@
+var fs = require('fs'),
+    path = require('path'),
+    chalk = require('chalk'),
+    glob = require('glob'),
+    compiler = require('ember-template-compiler'),
+    argv = require('yargs')
+        .usage('Usage: $0 -i [dir] -o [file]')
+        .demand(['i', 'o'])
+        .options('i', {
+            describe: 'input directory',
+            alias: 'input'
+        })
+        .options('o', {
+            describe: 'output file',
+            alias: 'output'
+        })
+        .argv;
+            
+            
+
+function buildTemplate(name, content) {
+    return "define('" + name +"', ['exports'], function(__exports__){ __exports__['default'] = " + content + "; });"
+}
+
+glob(path.join(argv.input, "**.hbs"), function(er, files) {
+    var output  = [];
+
+    files.forEach(function(f){
+        var body = fs.readFileSync(f, 'utf8'),
+            name = f.replace(/\.hbs/, ''),
+            template;
+
+        template = compiler.precompile(body).toString();
+
+        output.push(buildTemplate(name, template));
+        console.log(chalk.green('compiling: ' + f));
+    });
+
+    fs.writeFile(argv.output, output.join('\n'), function(err){
+        if (err) {
+            console.log(chalk.red(err));
+        }
+        else {
+            console.log(chalk.green('finished'));
+        }
+    }); 
+});
