4f941b45e6fb3aa5ff151617a25101395abed6b4
Matthew Philyaw <matthew.philyaw@gmail.com>
2014-05-04 22:45:17 -0400

    first stab, laid out the routes and created a default route object for each one.

--- app/router.js
+++ app/router.js
@@ -1,9 +1,22 @@
 var Router = Ember.Router.extend({
-    location: 'history'
+  location: 'history'
 });
 
 Router.map(function() {
+  /*
+   * At somepoint in the future perhaps we can have a world chooser
+   * that lets you select which world to build.
+   *
+   * this.route('worldChooser', { path: '/world' }); */
 
+  this.resource('world', { path: '/world/:name' }, function() {
+    this.resource('room', { path: '/room/:id' }, function() {
+      this.route('edit');
+      this.resource('exit', { path: '/exit/:direction' }, function() {
+        this.route('new');
+      });
+    });
+  });
 });
 
 export
