011789beb86b107c0cf93096b935b902ced393f8
Matthew Philyaw <matthew.philyaw@gmail.com>
2014-05-06 18:38:02 -0400

    need to support room/new

--- app/router.js
+++ app/router.js
@@ -11,6 +11,7 @@ Router.map(function() {
 
   this.resource('world', { path: '/world/:name' }, function() {
     this.resource('room', { path: '/room/:id' }, function() {
+      this.route('new');
       this.route('edit');
       this.resource('exit', { path: '/exit/:direction' }, function() {
         this.route('new');
